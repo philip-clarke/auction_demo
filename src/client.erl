237f1ee49527f541eb998eee25ec5e2696eb27c9
Philip Clarke <send2philip@gmail.com>
2015-02-27 21:43:22 +0000

    update for client behaviour

--- src/client.erl
+++ src/client.erl
@@ -6,16 +6,16 @@ start() ->
 
 handle_bid_request() ->
     receive
-        {Exchange, AuctionId} ->
-            send_bid_response(Exchange, AuctionId)
+        {From, AuctionId} ->
+            send_bid_response(From, AuctionId)
     end.
 
-send_bid_response(Exchange, AuctionId) ->
+send_bid_response(From, AuctionId) ->
     random:seed(now()),
     N = random:uniform(length(behaviour())),
     Behaviour = lists:nth(N, behaviour()),
     Bid = make_bid(Behaviour),
-    Exchange ! {self(), AuctionId, Bid}.
+    From ! {self(), AuctionId, Bid}.
 
 make_bid(return) ->
     Bid = random:uniform(5),
@@ -26,18 +26,27 @@ make_bid(sleep) ->
     timer:sleep(Delay * 1000),
     Bid;
 make_bid(work) ->
-    Delay = Bid = random:uniform(5),
+    Bid = random:uniform(5),
+    Delay = 3,
     fac(Delay * 800),
     Bid;
+make_bid(reds) ->
+    reds(10000);
+
 make_bid(crash) ->
     exit(bad_client).
 
 behaviour() ->
-    [return, sleep, work, crash].
-    %[work].
+    %[return, sleep, work, crash].
+    [return].
 
 fac(1) ->
     1;
 fac(N) ->
     N * fac(N-1).
 
+
+reds(0) ->
+    ok;
+reds(N) ->
+    reds(N -1).
