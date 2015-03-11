03a583e04c64cff20e570e44f810d34070c3c04c
Philip Clarke <send2philip@gmail.com>
2015-03-11 14:46:17 +0000

    auction ready for demo

--- src/auction.erl
+++ src/auction.erl
@@ -1,51 +1,28 @@
 -module(auction).
 -compile([export_all, debug_info]).
 
-start(NumClients) ->
-    Pid = erlang:spawn(fun() -> init(NumClients) end),
-    logger ! {create, Pid, 0}.
-
-init(NumClients) ->
-    process_flag(trap_exit, true),
-    AuctionId = erlang:now(),
-    Clients = spawn_clients(NumClients),
-    send_bid_requests(AuctionId, Clients),
-    handle_bid_responses(AuctionId, NumClients, []).
-
+%start(NumClients) ->
+%    Pid = erlang:spawn(fun() -> init(NumClients) end),
+%    logger ! {create, Pid, 0}.
+%
+%init(NumClients) ->
+%    AuctionId = erlang:now(),
+%    Clients = spawn_clients(NumClients),
+%    send_bid_requests(AuctionId, Clients),
+%    handle_bid_responses(AuctionId, []).
+%
 spawn_clients(0) ->
     [];
 spawn_clients(NumClients) ->
     Pid = spawn_link(client, start, []),
     [Pid | spawn_clients(NumClients - 1)].
 
-send_bid_requests(_AuctionId, []) ->
-    erlang:send_after(5000, self(), finished),
-    ok;
-send_bid_requests(AuctionId, [Client|Clients]) ->
-    Client ! {self(), AuctionId},
-    send_bid_requests(AuctionId, Clients).
 
-handle_bid_responses(AuctionId, NumClients) ->
-    handle_bid_responses(AuctionId, NumClients, []).
+send_bid_requests() ->
+    ok.
 
-handle_bid_responses(AuctionId, _NumClients = 0, Responses) ->
-    choose_winner(Responses, AuctionId);
-handle_bid_responses(AuctionId, NumClients, Responses) ->
-    receive
-        finished ->
-            choose_winner(Responses, AuctionId);
-        {_From, AuctionId, Bid} ->
-            logger ! {update, self(), length(Responses) + 1},
-            handle_bid_responses(AuctionId, NumClients - 1, [Bid|Responses]);
-        {'EXIT', _From, Reason} = Term when Reason /= normal ->
-            logger ! Term,
-            handle_bid_responses(AuctionId, NumClients - 1, Responses);
-        _Other ->
-            handle_bid_responses(AuctionId, NumClients, Responses)
-    end.
+handle_bid_responses() ->
+    ok.
 
-choose_winner([], AuctionId) ->
-    logger ! {no_winner, AuctionId};
-choose_winner(Responses, AuctionId) ->
-    Winner = lists:max(Responses),
-    logger !{winner, AuctionId, Winner}.
+choose_winner() ->
+    ok.
